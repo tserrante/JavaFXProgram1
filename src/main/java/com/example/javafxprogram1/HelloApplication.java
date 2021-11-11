fae9f7253be4e7d7eaa63c7f3b0a6aef810fa940
TS <tserrante@gmail.com>
2021-11-10 20:13:48 -0500

    Initial commit

---
+++ src/main/java/com/example/javafxprogram1/HelloApplication.java
@@ -0,0 +1,46 @@
+package com.example.javafxprogram1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application implements EventHandler<ActionEvent>  {
+
+
+    Button button;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        stage.setTitle("Title of the window");
+
+        button = new Button("This is a button");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 400);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent)
+    {
+
+    }
+}
