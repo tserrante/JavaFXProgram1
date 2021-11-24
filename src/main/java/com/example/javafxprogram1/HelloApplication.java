3748c99fdd0fce27d62a646ad409976d13e43a18
TS <tserrante@gmail.com>
2021-11-23 19:25:15 -0500

    check boxes and messages

--- src/main/java/com/example/javafxprogram1/HelloApplication.java
+++ src/main/java/com/example/javafxprogram1/HelloApplication.java
@@ -4,10 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import org.controlsfx.control.spreadsheet.Grid;
@@ -31,37 +28,39 @@ public class HelloApplication extends Application  {
     public void start(Stage primaryStage) throws IOException
     {
         window = primaryStage;
-        window.setTitle("Title of Window");
+        window.setTitle("Tom's Subs - Always Fresh");
 
-        // form
-        TextField nameInput = new TextField();
-        button = new Button("Click me");
-        button.setOnAction(e -> isInt(nameInput, nameInput.getText()));
+        //checkboxes
+        CheckBox box1 = new CheckBox("Bacon");
+        CheckBox box2 = new CheckBox("Tuna");
+        box2.setSelected(true);
+
+        button = new Button("Order Now!");
+        button.setOnAction(e -> handleOptions(box1, box2));
+        //layout
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(nameInput, button);
+        layout.getChildren().addAll(box1, box2, button);
 
         scene = new Scene(layout, 300, 250);
-
         window.setScene(scene);
         window.show();
     }
 
-    private boolean isInt(TextField nameInput, String text)
+    private void handleOptions(CheckBox box1, CheckBox box2)
     {
-        try
-        {
+        String message = "User Order: ";
 
-            int age = Integer.parseInt(text);
-            System.out.println("User is: " + age);
-            return true;
+        if(box1.isSelected())
+            message += "Bacon, ";
 
-        }
-        catch (NumberFormatException e)
-        {
-            System.out.println("Error: " + text + " is not a valid number");
-            return false;
-        }
+        if (box2.isSelected())
+            message += "Tuna";
+
+        if(!box2.isSelected() && !box1.isSelected())
+            message += "No options selected";
+
+        System.out.println(message);
     }
 
 
