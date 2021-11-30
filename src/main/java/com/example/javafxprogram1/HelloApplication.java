6d51170265b76caa05e8d9c246e7520a2d4e54c9
TS <tserrante@gmail.com>
2021-11-29 23:09:27 -0500

    action listener

--- src/main/java/com/example/javafxprogram1/HelloApplication.java
+++ src/main/java/com/example/javafxprogram1/HelloApplication.java
@@ -28,40 +28,28 @@ public class HelloApplication extends Application  {
     public void start(Stage primaryStage) throws IOException
     {
         window = primaryStage;
-        window.setTitle("Tom's Subs - Always Fresh");
+        window.setTitle("ChoiceBox Demo");
+        button = new Button("Click Me");
 
-        //checkboxes
-        CheckBox box1 = new CheckBox("Bacon");
-        CheckBox box2 = new CheckBox("Tuna");
-        box2.setSelected(true);
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
 
-        button = new Button("Order Now!");
-        button.setOnAction(e -> handleOptions(box1, box2));
-        //layout
+        // get items returns ObservableList object which you can add items to
+        choiceBox.getItems().add("Apples");
+        choiceBox.getItems().add("Bananas");
+        choiceBox.getItems().add("Grapes");
+        choiceBox.getItems().addAll("Oranges", "Blueberries", "Cherries", "Tomato");
+        choiceBox.setValue("Apple");
+
+        //listen for selection changes
+        choiceBox.getSelectionModel().selectedItemProperty().addListener((v, oldValue, newValue) -> System.out.println(newValue) );
+
+
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(box1, box2, button);
+        layout.getChildren().addAll(choiceBox, button);
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
     }
-
-    private void handleOptions(CheckBox box1, CheckBox box2)
-    {
-        String message = "User Order: ";
-
-        if(box1.isSelected())
-            message += "Bacon, ";
-
-        if (box2.isSelected())
-            message += "Tuna";
-
-        if(!box2.isSelected() && !box1.isSelected())
-            message += "No options selected";
-
-        System.out.println(message);
-    }
-
-
 }
